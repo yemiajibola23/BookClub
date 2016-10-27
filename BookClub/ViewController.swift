7c2a945a57bcf267072f2114bd0e0ef99411a63b
Yemi Ajibola <ya9p8@mst.edu>
2016-10-27 12:13:14 -0500

    Initial Commit

---
+++ BookClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookClub
+//
+//  Created by Yemi Ajibola on 10/27/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
